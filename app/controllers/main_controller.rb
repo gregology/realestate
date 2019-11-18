779db9e0fc2e0888da5b712fb29167dd20222736
Greg Clarke <greg.clarke@shopify.com>
2019-11-17 19:26:18 -0500

    adding more addresses to ignore

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,6 +4,9 @@ class MainController < ApplicationController
       .where("address NOT LIKE '%south church street%'") \
       .where("address NOT LIKE '%natures lane%'") \
       .where("address NOT LIKE '%meadow view lane%'") \
+      .where("address NOT LIKE '%butternut lane%'") \
+      .where("address NOT LIKE '%farm view lane%'") \
+      .where("address NOT LIKE '%hollow lane%'") \
       .where('ST_Distance(location, ST_SetSRID(ST_MakePoint(-77.208518, 43.820895), 4326)) < 40000') \
       .where("updated_at < ?", 36.hours.ago) \
       .order(created_at: :desc)
