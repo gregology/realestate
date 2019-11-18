00b411e20bbc6ce8f3207f58bab5a15616b84b10
Greg Clarke <greg.clarke@shopify.com>
2019-11-17 19:13:08 -0500

    removing unwanted ones

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,5 @@
 class MainController < ApplicationController
   def home
-    @properties = Property.where('ST_Distance(location, ST_SetSRID(ST_MakePoint(-77.208518, 43.820895), 4326)) < 50000').where("updated_at < ?", 1.days.ago).order(created_at: :desc)
+    @properties = Property.where("title NOT LIKE '%south church street%'").where('ST_Distance(location, ST_SetSRID(ST_MakePoint(-77.208518, 43.820895), 4326)) < 40000').where("updated_at < ?", 36.hours.ago).order(created_at: :desc)
   end
 end
