29252830b5060b1d47fd1b693d9982af4558b535
Greg Clarke <greg.clarke@shopify.com>
2019-11-17 19:16:19 -0500

    adding more ignores

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,11 @@
 class MainController < ApplicationController
   def home
-    @properties = Property.where("title NOT LIKE '%south church street%'").where('ST_Distance(location, ST_SetSRID(ST_MakePoint(-77.208518, 43.820895), 4326)) < 40000').where("updated_at < ?", 36.hours.ago).order(created_at: :desc)
+    @properties = Property \
+      .where("title NOT LIKE '%south church street%'") \
+      .where("title NOT LIKE '%natures lane%'") \
+      .where("title NOT LIKE '%meadow view lane%'") \
+      .where('ST_Distance(location, ST_SetSRID(ST_MakePoint(-77.208518, 43.820895), 4326)) < 40000') \
+      .where("updated_at < ?", 36.hours.ago) \
+      .order(created_at: :desc)
   end
 end
