0c07abb21b1920a723a242171e4d8bab417c82fa
Greg Clarke <greg.clarke@shopify.com>
2019-11-17 19:22:03 -0500

    adding details

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,9 @@
 class MainController < ApplicationController
   def home
     @properties = Property \
-      .where("title NOT LIKE '%south church street%'") \
-      .where("title NOT LIKE '%natures lane%'") \
-      .where("title NOT LIKE '%meadow view lane%'") \
+      .where("address NOT LIKE '%south church street%'") \
+      .where("address NOT LIKE '%natures lane%'") \
+      .where("address NOT LIKE '%meadow view lane%'") \
       .where('ST_Distance(location, ST_SetSRID(ST_MakePoint(-77.208518, 43.820895), 4326)) < 40000') \
       .where("updated_at < ?", 36.hours.ago) \
       .order(created_at: :desc)
