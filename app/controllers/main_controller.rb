1d5d247babe1eb607bb6d98aa73cb5aac7193574
Greg Clarke <greg.clarke@shopify.com>
2020-01-21 17:58:24 -0500

    yolo merge

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,17 +1,30 @@
 class MainController < ApplicationController
   def home
-    @properties = ZoloProperty \
-      .where("address NOT LIKE '%south church street%'") \
-      .where("address NOT LIKE '%natures lane%'") \
-      .where("address NOT LIKE '%meadow view lane%'") \
-      .where("address NOT LIKE '%butternut lane%'") \
-      .where("address NOT LIKE '%farm view lane%'") \
-      .where("address NOT LIKE '%hollow lane%'") \
-      .where("address NOT LIKE '%south front street%'") \
-      .where("address NOT LIKE '%summer village%'") \
-      .where('ST_Distance(location, ST_SetSRID(ST_MakePoint(-77.208518, 43.820895), 4326)) < 40000') \
-      .where("updated_at > ?", 24.hours.ago) \
-      .order(created_at: :desc)
+    sql = """
+      SELECT
+        zp.title,
+        zp.photo_url,
+        zp.created_at,
+        zp.updated_at,
+        ST_Y(ST_Transform(CAST(zp.location AS GEOMETRY), 4326)) AS lat,
+        ST_X(ST_Transform(CAST(zp.location AS GEOMETRY), 4326)) AS lon,
+        ST_Y(ST_Transform(CAST(zp.location AS GEOMETRY), 3857)) + 400 AS north,
+        ST_X(ST_Transform(CAST(zp.location AS GEOMETRY), 3857)) + 400 AS east,
+        ST_Y(ST_Transform(CAST(zp.location AS GEOMETRY), 3857)) + 400 AS south,
+        ST_X(ST_Transform(CAST(zp.location AS GEOMETRY), 3857)) + 400 AS west,
+        zp.price_history
+      FROM
+        zolo_properties zp
+        JOIN remax_properties rp ON ST_Distance(zp.location, rp.location) < 10
+      WHERE
+        ST_Distance(zp.location, ST_SetSRID(ST_MakePoint(-77.208518, 43.820895), 4326)) < 40000
+        AND zp.updated_at > NOW() - INTERVAL '24 HOURS'
+        AND zp.latest_list_price BETWEEN 100000 AND 1200000
+        AND lot_size > 108900
+      ORDER BY zp.created_at DESC
+    """
+
+    @properties = records_array = ActiveRecord::Base.connection.execute(sql)
   end
 
   def all
