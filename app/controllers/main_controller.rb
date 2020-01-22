bc188fc3f40591073bba433133b2c9a75b368dc7
Greg Clarke <greg.clarke@shopify.com>
2020-01-21 22:49:52 -0500

    better query

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -14,15 +14,16 @@ class MainController < ApplicationController
         ST_Y(ST_Transform(CAST(zp.location AS GEOMETRY), 3857)) + 400 AS south,
         ST_X(ST_Transform(CAST(zp.location AS GEOMETRY), 3857)) + 400 AS west,
         zp.price_history,
-        zp.latest_list_price
+        zp.latest_list_price,
+        ROUND((rp.lot_size / 43560.0)::numeric,2) AS acres
       FROM
         zolo_properties zp
         JOIN remax_properties rp ON ST_Distance(zp.location, rp.location) < 10
       WHERE
         ST_Distance(zp.location, ST_SetSRID(ST_MakePoint(-77.208518, 43.820895), 4326)) < 40000
         AND zp.updated_at > NOW() - INTERVAL '24 HOURS'
-        AND zp.latest_list_price BETWEEN 100000 AND 1200000
-        AND lot_size > 108900
+        AND zp.latest_list_price BETWEEN 100000 AND 1500000
+        AND lot_size > 43560
       ORDER BY zp.created_at DESC
     """
 
