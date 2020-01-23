cb4dc89f36284f2170cb8ab3a94cbf2bdad2e140
Greg Clarke <greg.clarke@shopify.com>
2020-01-23 17:36:32 -0500

    cleaning up

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,10 +9,10 @@ class MainController < ApplicationController
         zp.updated_at,
         ST_Y(ST_Transform(CAST(zp.location AS GEOMETRY), 4326)) AS lat,
         ST_X(ST_Transform(CAST(zp.location AS GEOMETRY), 4326)) AS lon,
-        ST_Y(ST_Transform(CAST(zp.location AS GEOMETRY), 3857)) + 400 AS north,
-        ST_X(ST_Transform(CAST(zp.location AS GEOMETRY), 3857)) + 400 AS east,
-        ST_Y(ST_Transform(CAST(zp.location AS GEOMETRY), 3857)) + 400 AS south,
-        ST_X(ST_Transform(CAST(zp.location AS GEOMETRY), 3857)) + 400 AS west,
+        ST_Y(ST_Transform(CAST(zp.location AS GEOMETRY), 3857)) + 400 AS bbox_3857_north,
+        ST_X(ST_Transform(CAST(zp.location AS GEOMETRY), 3857)) + 400 AS bbox_3857_east,
+        ST_Y(ST_Transform(CAST(zp.location AS GEOMETRY), 3857)) + 400 AS bbox_3857_south,
+        ST_X(ST_Transform(CAST(zp.location AS GEOMETRY), 3857)) + 400 AS bbox_3857_west,
         zp.price_history,
         zp.latest_list_price,
         ROUND((rp.lot_size / 43560.0)::numeric,2) AS acres
@@ -21,7 +21,7 @@ class MainController < ApplicationController
         JOIN remax_properties rp ON ST_Distance(zp.location, rp.location) < 10
       WHERE
         ST_Distance(zp.location, ST_SetSRID(ST_MakePoint(-77.208518, 43.820895), 4326)) < 40000
-        AND zp.updated_at > NOW() - INTERVAL '24 HOURS'
+        AND zp.updated_at > NOW() - INTERVAL '48 HOURS'
         AND zp.latest_list_price BETWEEN 100000 AND 1500000
         AND lot_size > 43560
       ORDER BY zp.created_at DESC
