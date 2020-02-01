ebf809daafd3f6415f9553c862b89de3269284e3
Greg Clarke <greg.clarke@shopify.com>
2020-02-01 17:40:02 -0500

    typo

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,9 +4,9 @@ class MainController < ApplicationController
       .where("updated_at > ?", 48.hours.ago) \
       .where("acres > ? OR acres = -1", 1.5) \
       .where(list_price: 100000..1500000)
-      
+
     @latest_zolo          = ZoloProperty.maximum('updated_at').to_date
-    @latest_reman         = RemaxProperty.maximum('updated_at').to_date
+    @latest_remax         = RemaxProperty.maximum('updated_at').to_date
     @latest_purple_bricks = PurpleBricksProperty.maximum('updated_at').to_date
   end
 
